f17e5e9c6c6555c20d9c49a9993961d051e98d78
Gaurav Prakash <g.prakash@tbauctions.com>
2024-03-26 14:19:13 +0100

    forms

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.sass'
+  styleUrl: './overview.component.sass',
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  // fName: string | null = null;
+  // lName = null;
+  // phNumber = 0;
+  // email = '';
+  // isAgreed = false;
 
+  overviewForm!: FormGroup;
+
+  // fb: FormBuilder = inject(FormBuilder)
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.overviewForm = new FormGroup({
+      fName: new FormControl(''),
+      lName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      phNumber: new FormControl(null),
+      email: new FormControl(null),
+      isAgreed: new FormControl(false),
+    });
+  }
+
+  onSubmitForm(): void {
+    console.log('for submitted!', this.overviewForm.value);
+
+    // this.phNumber = 0;
+
+    // console.log('lName', this.lName);
+    // console.log('fname', this.fName);
+
+    // pattern = '[0-9]{3}-[0-9]{2}-[0-9]{3}';
+  }
 }
